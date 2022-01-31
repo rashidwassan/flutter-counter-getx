680c13fb6eec9012b41f3a3ddaa1d0bc61078dfe
Rashid Wassan <rashidwassaan@gmail.com>
2022-02-01 00:41:20 +0500

    add floating action button

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,13 +13,19 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Counter")),
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: _counterController.increment),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             // using the instance of controller to acces count's value
-            Obx(() => Text("Clicks: ${_counterController.count}")),
+            Obx(() => Text(
+                  "Clicks: ${_counterController.count}",
+                  style: const TextStyle(fontSize: 20),
+                )),
             // adding a bit of spacing (vertical)
             const SizedBox(height: 24),
             ElevatedButton(
