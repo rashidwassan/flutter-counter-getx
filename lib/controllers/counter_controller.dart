9fe176add8ca9043caf1643863d1d5b1106273cd
Rashid Wassan <rashidwassaan@gmail.com>
2022-01-31 23:56:51 +0500

    add comments in counter controller

--- lib/controllers/counter_controller.dart
+++ lib/controllers/counter_controller.dart
@@ -1,6 +1,10 @@
 import 'package:get/get.dart';
 
+// counter controller will be responsible for state management
 class CounterController extends GetxController {
+  // counter state, made it observable.
   var count = 0.obs;
+
+  // increment function
   increment() => count++;
 }
